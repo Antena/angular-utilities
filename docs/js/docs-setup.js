ae93f8b85944c60b698b05e1cff3e4418756f9c7
Denise Fernandez <denise@antena.co>
2017-01-05 16:57:58 -0300

    commit docs folder for github pages

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,72 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angular-utilities.misc.filter:trusted",
+      "shortName": "trusted",
+      "type": "filter",
+      "moduleName": "angular-utilities.misc",
+      "shortDescription": "Allows you to register html strings as trusted (so that angular allows rendering it) on the fly.",
+      "keywords": "allows angular angular-utilities api filter fly function html misc register rendering string strings text trusted"
+    },
+    {
+      "section": "api",
+      "id": "angular-utilities.performance.factory:WatchUtilities",
+      "shortName": "WatchUtilities",
+      "type": "service",
+      "moduleName": "angular-utilities.performance",
+      "shortDescription": "Collection of utilitary functions to help with angular performance issues.",
+      "keywords": "$digest angular angular-utilities angularjs api attrs callback changed changes collection cycle demo deregister digest execute executed expression factory function functions harmed help https invoked issues js listener making oldval oncondition operations org performance registers scope service trigger triggering unwatch utilitary watch watcher watchers watchexpression watchindependently watchutilities"
+    },
+    {
+      "section": "api",
+      "id": "angular-utilities.scroll.directive:isolateScrollingWhen",
+      "shortName": "isolateScrollingWhen",
+      "type": "directive",
+      "moduleName": "angular-utilities.scroll",
+      "shortDescription": "Isolates the scrolling event within this directive&#39;s element on a given condition.",
+      "keywords": "$apply $document $scope angular angular-utilities api avoid background-color bad behaviour bfe3ff border box bubbling c0334e checkbox class color condition container content css d5f5ff default demo demo-container demo-controls directive documentscrollcount doisolate don element enable evaluates event events example example-directive exampledirective expression false falsy fff057 fff492 fixedtree form-control form-inline function good hodor html isolate-scrolling-example isolate-scrolling-when isolated isolates isolatescrollingwhen isolation js lab-tree link main margin-bottom max-height module mouse move ng-model overflow-y padding reached reaching readonly received restored restrict return scroll scrolling solid start styles test text treeisfixed truthy type value-within-range-card vm watch window yellow"
+    },
+    {
+      "section": "api",
+      "id": "angular-utilities.text.filter:capitalize",
+      "shortName": "capitalize",
+      "type": "filter",
+      "moduleName": "angular-utilities.text",
+      "shortDescription": "Allows you to make capital the first letter of words in a given string. Defaults to just transforming",
+      "keywords": "allows angular-utilities api brown capital capitalize configured default defaults dog filter fox function html indicates input jumps lazy letter module options quick string text transform transformed transforming word"
+    },
+    {
+      "section": "api",
+      "id": "angular-utilities.text.filter:ellipsis",
+      "shortName": "ellipsis",
+      "type": "filter",
+      "moduleName": "angular-utilities.text",
+      "shortDescription": "Appends an ellipsis to the given string.",
+      "keywords": "angular-utilities api appends brown dog ellipsis filter fox function html input jumps lazy module quick string text transformed"
+    },
+    {
+      "section": "api",
+      "id": "angular-utilities.text.filter:semicolon",
+      "shortName": "semicolon",
+      "type": "filter",
+      "moduleName": "angular-utilities.text",
+      "shortDescription": "Adds a semicolon at the end of the given string.",
+      "keywords": "add adds angular-utilities api brown dog filter filters fox function generic html input jumps lazy module puntuation quick semicolon string strings text transformed translatable translations"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": false,
+  "startPage": "/api",
+  "scripts": [
+    "bundle.js",
+    "angular-animate.min.js",
+    "prism.js"
+  ]
+};
